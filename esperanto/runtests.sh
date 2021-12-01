0f15da3459e7692ca45f54cb737e8ee717863760
David Callahan <callahan@reservoir.com>
2021-12-01 15:07:14 +0000

    Some cleanup and normalizations of the regression tests (NFC)

--- esperanto/runtests.sh
+++ esperanto/runtests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DIR=$(realpath $(dirname $0))
+set -ex
 cd $DIR
 ../build/bin/llvm-lit ../clang/test/CodeGen/esperanto ../llvm/test/CodeGen/RISCV
-
+cd python
+bash runtests.sh
+cd ../test
+bash runtests.sh
