6dfb9211eea250ee34fe310a74fb979099402451
Pau Farre <pau.farre@esperantotech.com>
2023-11-08 16:36:38 +0000

    [SW-19216] Build esperanto-llvm pkg with gcc11

--- esperanto/ci_build.py
+++ esperanto/ci_build.py
@@ -21,6 +21,7 @@ def main():
     build = Packager(ci_build=True)
     build.add_package(conanfile_path)
     build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
+    build.add_configuration("default", "linux-ubuntu22.04-x86_64-gcc11-release")
     build.report()
 
     build.run()
